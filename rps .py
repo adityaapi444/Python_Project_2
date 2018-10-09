d13534ebd8bc0841808bd95a5217985cb2f67c7e
Aditya Apishte <adityaapi444@gmail.com>
2018-10-09 21:04:17 +0530

    v2

--- rps .py
+++ rps .py
@@ -1,22 +1,19 @@
-#import Packages
 
-import random
+import random                                                                                               #import Packages
 import getpass
 import time
 
-
-#class created for RPS
-
-class game:
+class game:                                                                                                 #class created for RPS
+    
     'This is Rock Paper Scissor game.Rock crushes scissor.Paper covers rock.Scissor cuts Paper.'
     
-#initialized with mode selection
     
-    def __init__(self):
+    def __init__(self):                                                                                     #initialized with mode selection
+        
         print('***************************************')
-        self.choices={1:'Rock',2:'Paper',3:'Scissor'}
+        self.choices={1:'Rock',2:'Paper',3:'Scissor'}                                                       #choices defined
         try:
-            self.mode=int(input("1 : Vs Computer , 2 : Two Player\n\nSelect Mode :"))
+            self.mode=int(input("1 : Vs Computer , 2 : Two Player\n\nSelect Mode :"))                       #mode selection
             if self.mode not in (1,2):
                 raise ValueError
             
@@ -25,66 +22,71 @@ class game:
             self.__init__()
         else:
             self.select_weapon()
-            
-#define function to select weapon
-            
-    def select_weapon(self):
+
+                   
+    def select_weapon(self):                                                                                #define func. to select weapon
+        
         try:
             print('***************************************')
             print('1: Rock  2: paper 3: Scissor\n\n')
-            self.player1= int(getpass.getpass(prompt = "For player 1\n\nYour Choice : "))
+            self.player1= int(getpass.getpass(prompt = "For player 1\n\nYour Choice : "))                   #player 1 select weapon
             if self.mode==2:
-                self.player2 = int(getpass.getpass(prompt = "\nFor player 2\n\nYour Choice : "))
+                self.player2 = int(getpass.getpass(prompt = "\nFor player 2\n\nYour Choice : "))            #player 2 select weapon(when mode = 2)
                 if self.player2 not in (1,2,3):
                     raise ValueError
+                
             if self.player1 not in (1,2,3):
                 raise ValueError
+            
         except ValueError:
             print('Choose the right one')
             self.select_weapon()
+            
         else:
                 if self.mode==1:
-                    self.player2=random.choice(list(self.choices.keys()))
+                    self.player2=random.choice(list(self.choices.keys()))                                   #player 2(computer) select weapon(when mode=1)
                 self.rps(self.player1,self.player2)
             
-#define func to check the winner
 
-    def rps(self,p1,p2):
+    def rps(self,p1,p2):                                                                                    #define func. to check the winner
         dif=p1-p2
-        print('\n*************' ,end='');time.sleep(0.5);print('*************', end='');time.sleep(0.5);
+        print('\n*************' ,end='');time.sleep(0.5);print('*************', end='');time.sleep(0.5);    #sleep() used
         print('******' ,end='');time.sleep(0.5);print('*******', end='\n')
-        if dif==0:
+        
+        if dif==0:                                                                                          #to check whether match is tie or not 
             print("Its a Tie!!!\nBoth chose",self.choices[p2])
-        elif dif==1 or dif==-2:
+        elif dif==1 or dif==-2:                                                                             #to check  result 
             print('Congrats Player 1 Won!!!\nPlayer 2 chose',self.choices[p2])
         else:
             if self.mode==1:
                 print('You Lost!!!Better Luck next Time!!\nComputer chose',self.choices[p2])
             else:
                 print('Congrats Player 2 Won!!!\nPlayer 1 chose',self.choices[p1])
+                
         self.play_again()
         
-#define func for next round
         
-    def play_again(self):
+    def play_again(self):                                                                                   #define func for next round
+        
         while (True):
             print('\n***************************************')
             d=input("To play again(y/n) or To Change mode(m)")
+            
             while d.lower() not in('y','n','m','yes','no',):
-                d=input("To play again(y/n) orTo Change mode(m)")
-            if d.lower()=='n':
-                    print('\nThanks for playing.\tBe happy')
+                d=input("To play again(y/n) or To Change mode(m)")
+                
+            if d.lower()=='n':                                                                              #exit
+                    print('\nThanks for playing!!!!Be happy!!')
                     time.sleep(2)
                     break
-            if d.lower()=='m':
+            if d.lower()=='m':                                                                              #to change the mode
                 self.__init__()
                 break
-            self.select_weapon()
+            self.select_weapon()                                                                            #to play in same mode
             break
 
-#here the game begin
 
-print('Welcome to the Rock Paper Scissor Game')
+print('Welcome to the Rock Paper Scissor Game')                                                             #here the game begin
 print('***************************************')
 print('Rock crushes scissor.')
 print('          Paper covers rock.')
