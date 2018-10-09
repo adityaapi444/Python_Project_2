ab57f88dc2e28411507a6376cf917a32809b13f7
Aditya Apishte <adityaapi444@gmail.com>
2018-10-09 20:54:19 +0530

    wordbank for hangman

---
+++ for making wordbank.py
@@ -0,0 +1,11 @@
+fo=open('wordbank.txt','r')
+print('done........')
+print('   ____')
+print('  |    |   ')   
+print('  |    o  ')    
+print('  |   /|\ ')
+print('  |    |')     
+print('  |   / \ ')
+print('  |
+print(' _|__________')
+
